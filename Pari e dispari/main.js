88436487b4fa5235a5bbee5109283c8ee3798e51
Dario Becchio <dabek1986@gmail.com>
2023-12-01 19:47:15 +0100

    create two folders

--- Pari e dispari/main.js
+++ Pari e dispari/main.js
@@ -1,38 +1,3 @@
-/*
-Palidroma
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma
-*/
-/*ESERCIZIO 1*****
-
-// Creare una variabile dove chiediamo all'utente di inserire una parola
-
-const word = prompt('Scrivi una parola');
-
-//Dichiaro la variabile
-analizeWord(word)
-
-
-//Creare una funzione che compara la parola inserita con il suo contrario e verifica se sono uguali
-
-function analizeWord (word) {
-    let split_word = word.split('');
-    console.log(split_word);
-    let split_word_1 = split_word.reverse()
-    console.log( split_word_1);
-    let reverse_word = split_word_1.join('')
-    console.log(reverse_word);
-    if (word === reverse_word){
-        return console.log("E' palindroma");
-    } else {
-        return console.log("Non è palindroma");
-    }
-
-
-      
-}
-*****************************************************************/
-
 
 /*
 Pari e Dispari
@@ -43,7 +8,7 @@ Dichiariamo chi ha vinto.
 */
 
 
-/*****Esercizio 2  
+  
 //Creo una costante con un prompt che chiede di scegliere pari o dispari
 
 const user_choice = prompt ('Scrivi uno tra pari o dispari');
